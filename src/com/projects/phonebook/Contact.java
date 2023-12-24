9a1f68f1ffb41a4f578132f5ad99334ee950390b
user <sajadhnk@gmail.com>
2023-12-24 06:46:45 +0330

    this is start point of commit

---
+++ src/com/projects/phonebook/Contact.java
@@ -0,0 +1,6 @@
+package com.projects.phonebook;
+
+public class Contact {
+    public String name;
+    public String number;
+}
---
+++ src/com/projects/phonebook/PhoneBook.java
@@ -0,0 +1,81 @@
+package com.projects.phonebook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PhoneBook {
+//    private static ArrayList<String> names = new ArrayList<>();
+//    private static ArrayList<String> numbers = new ArrayList<>();
+    private static ArrayList<Contact> contacts = new ArrayList<>();
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            printMenu();
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            System.out.println("--------------------");
+            scanner.nextLine();
+            // System.out.println(choice);
+
+            switch (choice) {
+                case 1:
+                    addContact(scanner);
+                    break;
+                case 2:
+                    printAllContacts();
+                    break;
+                case 3:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("Invalid User Input.");
+            }
+        } while (choice != 3);
+        scanner.close();
+    }
+
+    private static void printMenu() {
+        System.out.println("\n--------Menu--------");
+        System.out.println("1. Add contacts");
+        System.out.println("2. Print all contacts");
+        System.out.println("3. Exit");
+        System.out.println("--------------------");
+    }
+
+    private static void printAllContacts() {
+        if(contacts.isEmpty()){
+            System.out.println("Phonebook is empty!");
+        }else{
+            System.out.println("All Contacts:");
+            System.out.println("--------------------");
+            for (int i = 0; i < contacts.size(); i++) {
+                //int j = i + 1;
+                System.out.print("Contact No. " + (i + 1) );
+                System.out.print(" Name is: " + contacts.get(i).name +" and ");
+                System.out.println("Number is: " + contacts.get(i).number);
+                System.out.println("--------------------");
+            }
+        }
+        if(!contacts.isEmpty()) {
+            System.out.println("All contact's Names and Numbers printed successfully!");
+        }
+    }
+
+    private static void addContact(Scanner scanner){
+        Contact newContact = new Contact();
+        System.out.println("Enter contact's name: ");
+        newContact.name = scanner.nextLine();
+//        String name = scanner.nextLine();
+        System.out.println("Enter contact's number: ");
+        newContact.number = scanner.nextLine();
+//        String number = scanner.nextLine();
+//        names.add(name);
+//        numbers.add(number);
+        contacts.add(newContact);
+        System.out.println("Contact added successfully!");
+        System.out.println("--------------------");
+    }
+}
